bddb99cc74b00af15104c9cba89761ac6eb0392c
korede456 <koredethompson456@gmail.com>
2024-06-23 10:04:43 +0100

    udated version

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 require("dotenv").config();
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
